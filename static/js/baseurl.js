8465e4ec1bac1bbabd6bca27c54f30ae208510f5
nihao2219713860 <2219713860@qq.com>
2023-05-17 22:10:58 +0800

    添加了商品管理系统前端的所有的页面

--- static/js/baseurl.js
+++ static/js/baseurl.js
@@ -2,6 +2,7 @@
 var baseUrl="http://localhost:8080/";
 var webSocketBaseurl="ws://localhost:8080/";
 
-// 云主机
-// var baseUrl="http://101.43.235.98:8080/";
-// var webSocketBaseurl="ws://101.43.235.98:8080/";
+
+// // 云主机：当前端服务器与后端服务器不在一个上面的时候
+// var baseUrl="http://101.42.176.90:8080/";
+// var webSocketBaseurl="ws://101.42.176.90:8080/";
